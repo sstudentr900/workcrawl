f2395d39a291723363364a84f33f45e317cd87a9
John Doe <johndoe@example.com>
2023-10-14 22:35:23 +0800

    01

--- controllers/crawl.js
+++ controllers/crawl.js
@@ -2,7 +2,7 @@ const { dbQuery,dbInsert,dbUpdata,dbDelete,timeFn } = require('../tools/db')
 async function search(req, res) {
   let date = await timeFn({day:'-5'})
   date= date['date']
-  const rows = await dbQuery( 'SELECT cu.id,cu.storename,cu.storeurl,cu.keyword,cu.nokeyword,(SELECT COUNT(id) FROM work WHERE work.crawlerurl_id = cu.id) AS total,(SELECT COUNT(id) FROM work WHERE work.crawlerurl_id = cu.id AND work.time >= ?) AS amount FROM crawlerurl cu',[date])
+  const rows = await dbQuery( 'SELECT cu.id,cu.storename,cu.storeurl,cu.keyword,cu.nokeyword,(SELECT COUNT(id) FROM work WHERE work.crawlerurl_id = cu.id) AS total,(SELECT COUNT(id) FROM work WHERE work.crawlerurl_id = cu.id AND work.time >= ?) AS amount FROM crawlerurl cu where deletes= ?',[date,'n'])
   res.render('crawl',{
     'active': 'crawl',
     'data': {
@@ -16,7 +16,7 @@ async function add(req, res) {
   const storeurl = params.storeurl
   const keyword = params.keyword
   const nokeyword = params.nokeyword
-  console.log(params,storename,storeurl,keyword,nokeyword)
+  // console.log(params,storename,storeurl,keyword,nokeyword)
   if(!storename || !storeurl || !keyword || !nokeyword){
     // console.log(`來源資料錯誤:${storename}-${storeurl}-${keyword}-${nokeyword}`)
     res.json({result:'false',message:'來源資料錯誤'})
@@ -34,13 +34,14 @@ async function add(req, res) {
 }
 async function delet(req, res) {
   const id = req.params.id
-  console.log(`id:${id}`)
+  // console.log(`id:${id}`)
   if(!id){
-    console.log(`來源資料錯誤:${params}`)
+    // console.log(`來源資料錯誤:${params}`)
     res.json({result:'false',message:'來源資料錯誤'})
     return false;
   }
-  const rows = await dbDelete('stock',id)
+  // const rows = await dbDelete('crawlerurl',id)
+  const rows = await dbUpdata('crawlerurl',{'deletes':'y'},id)
   if(rows){
     res.json({result:'true',message:'刪除成功'})
   }else{
@@ -55,7 +56,7 @@ async function put(req, res) {
   const nokeyword = req.params.nokeyword
   const id = req.params.id
   console.log(params,storename,storeurl,keyword,nokeyword,id)
-  await dbUpdata('stock',{'storename':storename,'storeurl':storeurl,'keyword':keyword,'nokeyword':nokeyword},id)
+  await dbUpdata('crawlerurl',{'storename':storename,'storeurl':storeurl,'keyword':keyword,'nokeyword':nokeyword},id)
   res.json({result:'true',message: '修改成功'})
 }
 module.exports = { 
