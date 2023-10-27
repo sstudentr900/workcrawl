c7ecf1e8100db54e4eb7ea9dbcd8482155396781
sstudentr123 <sstudentr123700900@yahoo.com.tw>
2023-10-27 17:56:34 +0800

    01

--- tools/crawler.js
+++ tools/crawler.js
@@ -1,15 +1,16 @@
 const CronJob = require('cron').CronJob;
 const { dbQuery,dbInsert,dbUpdata,dbDelete } = require('./db')
-const {crawlerFB} = require("./tools/crawlerFB.js"); //爬蟲FB
-const {crawlerOHF} = require("./tools/crawlerOHF.js"); //爬蟲104
-const {crawlerBEAR} = require("./tools/crawlerBEAR.js"); //爬蟲518
-const {crawlerOTE} = require("./tools/crawlerOTE.js"); //爬蟲1111
+const {crawlerFB} = require("./crawlerFB.js"); //爬蟲FB
+const {crawlerOHF} = require("./crawlerOHF.js"); //爬蟲104
+const {crawlerBEAR} = require("./crawlerBEAR.js"); //爬蟲518
+const {crawlerOTE} = require("./crawlerOTE.js"); //爬蟲1111
 new CronJob({
-  cronTime: '1 28 13 * * *',//時段(秒/分/時)
+  cronTime: '1 12 17 * * *',//時段(秒/分/時)
   onTick: async function () { //執行程式
     console.log(`開始執行爬蟲排程作業： ${new Date()}`);
     const rows = await dbQuery( 'SELECT * from crawlerurl where deletes = ?',['n'])
-    if(!rows){console.log(`cronJob_rows沒有資料跳出`);return false;}
+    // console.log(rows.length)
+    if(!rows.length){console.log(`cronJob_rows沒有資料跳出`);return false;}
     for (const row of rows) {
       if(row['storeurl'].includes('facebook')){
         await crawlerFB(row)
@@ -23,7 +24,7 @@ new CronJob({
         console.log('cronJob_storeurl沒有資料跳出 error')
       }
     }
-    console.log('排程作業執行完畢！');
+    console.log(`排程作業執行完畢！ ${new Date()}`);
   },
   start: true, //自動
   timeZone: 'Asia/Taipei',//時區
